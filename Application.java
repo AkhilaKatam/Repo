63457ed1ba477f8e2a655e6da4d40df5b770d807
AkhilaKatam <akhilakatam9@gmail.com>
2023-02-07 13:06:46 +0530

    Feature 201 changes done by me

--- Application.java
+++ Application.java
@@ -4,3 +4,26 @@ public class Main {
     System.out.println("The length of the txt string is: " + txt.length());
   }
 }
+
+
+#Feature201 changes done by me on the services module on Feb2023
+
+class Main {
+
+// create a method
+  public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
+
+  public static void main(String[] args) {
+    int result;
+
+    // call the method
+    // store returned value to result
+    result = square(10);
+
+    System.out.println("Squared value of 10 is: " + result);
+  }
+}
