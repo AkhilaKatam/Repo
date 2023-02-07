c0262bc359de457d6df2490d85f4b96f18a62dfc
AkhilaKatam <akhilakatam9@gmail.com>
2023-02-07 13:21:20 +0530

    Feature202 changes done by harsha

--- Application.java
+++ Application.java
@@ -4,3 +4,13 @@ public class Main {
     System.out.println("The length of the txt string is: " + txt.length());
   }
 }
+
+#Feature 202 changes has been done by harsha on feb2023
+class Main {
+
+  // create a method
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+    // return value
+    return sum;
+  }
